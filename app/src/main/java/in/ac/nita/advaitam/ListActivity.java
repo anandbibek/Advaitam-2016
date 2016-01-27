2e6f67cf2aa0a1c54bb7231f84cbfeb30bd380ca
anandbibek <ananda.bibek@gmail.com>
2016-01-27 13:33:03 +0530

    Fix loading data

--- app/src/main/java/in/ac/nita/advaitam/ListActivity.java
+++ app/src/main/java/in/ac/nita/advaitam/ListActivity.java
@@ -3,6 +3,7 @@ package in.ac.nita.advaitam;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -24,6 +25,7 @@ public class ListActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     MyRecyclerAdapter adapter;
     RequestQueue requestQueue;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +38,7 @@ public class ListActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
         requestQueue = CustomVolley.getInstance(this).getRequestQueue();
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipeRefresh);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,33 +48,36 @@ public class ListActivity extends AppCompatActivity {
             }
         });
         feedItems = new ArrayList<>();
-        //insertDummyData();
-        loadData();
+        if(savedInstanceState==null)
+            loadData();
 
     }
 
     private void loadData(){
-        String url = getIntent().getStringExtra("URL");
-        StringRequest request = new StringRequest(url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        //Log.wtf("TEXT", response);
-                        feedItems = Parser.parseDetails(response);
-                        adapter = new MyRecyclerAdapter(ListActivity.this,feedItems);
-                        recyclerView.setAdapter(adapter);
 
-                        //mSwipeRefreshLayout.setRefreshing(false);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        //mSwipeRefreshLayout.setRefreshing(false);
-                        //Snackbar.make(root, "Something went wrong", Snackbar.LENGTH_SHORT);
-                    }
-                });
-        requestQueue.add(request);
-        //mSwipeRefreshLayout.setRefreshing(true);
+        String url = getIntent().getStringExtra("URL");
+
+        if(url!=null) {
+            StringRequest request = new StringRequest(url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            //Log.wtf("TEXT", response);
+                            feedItems = Parser.parseDetails(response);
+                            adapter = new MyRecyclerAdapter(ListActivity.this, feedItems);
+                            recyclerView.setAdapter(adapter);
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            swipeRefreshLayout.setRefreshing(false);
+                            Snackbar.make(recyclerView, "Something went wrong", Snackbar.LENGTH_SHORT);
+                        }
+                    });
+            requestQueue.add(request);
+            swipeRefreshLayout.setRefreshing(true);
+        }
     }
 }
---
+++ app/src/main/java/in/ac/nita/advaitam/MyRefreshLayout.java
@@ -0,0 +1,42 @@
+package in.ac.nita.advaitam;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+
+/**
+ * This is required to show loading animation during first sync
+ * setRefreshing doesn't work before onMeasure is called
+ * So we are setting flags to call setRefreshing as soon as onMeasure is done first time
+ */
+public class MyRefreshLayout extends SwipeRefreshLayout {
+
+    private boolean mMeasured = false;
+    private boolean mPreMeasureRefreshing = false;
+
+    public MyRefreshLayout(Context context) {
+        super(context);
+    }
+
+    public MyRefreshLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (!mMeasured) {
+            mMeasured = true;
+            setRefreshing(mPreMeasureRefreshing);
+        }
+    }
+
+    @Override
+    public void setRefreshing(boolean refreshing) {
+        if (mMeasured) {
+            super.setRefreshing(refreshing);
+        } else {
+            mPreMeasureRefreshing = refreshing;
+        }
+    }
+}
